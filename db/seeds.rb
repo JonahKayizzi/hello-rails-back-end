fca39937183c1fda47b6ac44356cd30b7c6f8284
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-02-23 19:08:11 +0300

    Add migrations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  "Good morning!",
+  "Bonjour.",
+  "Bonjourno.",
+  "Ni hao!",
+  "Hola!"
+]
+
+greetings.each { |greeting| Greetings.create(greeting: greeting) }
